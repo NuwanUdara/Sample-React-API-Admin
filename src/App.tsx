475248f8aec70bce6310ba4a3ed685ac2f6bdae3
nuwanudara <nuwanudara36@gmail.com>
2023-04-21 14:12:07 +0530

    Added Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Admin, Resource} from "react-admin";
+import { Admin, Resource, EditGuesser} from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import {UserList} from "./users"
-import {PostList} from "./posts"
+import {PostList, PostEdit, PostCreate} from "./posts"
+
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-
+// {< Resource name="posts" list={PostList} />}
 const App = () => (
    <Admin dataProvider={dataProvider}>
-     {< Resource name="users" list={UserList} />}
-     {< Resource name="posts" list={PostList} />}
+     {< Resource name="users" list={UserList} recordRepresentation="name" icon={UserIcon} />}
+    
+     {< Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />}
    </Admin>
   );
 
