1457789d3147a62eb05dd3352620f3cdee9f3902
nuwanudara <nuwanudara36@gmail.com>
2023-04-21 14:11:53 +0530

    Added Post editing and creation.

--- src/posts.tsx
+++ src/posts.tsx
@@ -12,13 +12,19 @@
 // );
 
 
-import { useMediaQuery } from "@mui/material";
-import { List, SimpleList, Datagrid, TextField} from "react-admin";
+import { useMediaQuery} from "@mui/material";
+import { List, SimpleList, Datagrid, TextField, ReferenceField, Edit, EditButton, ReferenceInput, SimpleForm, TextInput, Create} from "react-admin";
+
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users" />,
+];
+
 
 export const PostList = () => {
   const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   return (
-    <List>
+    <List >
       {isSmall ? (
         <SimpleList
           primaryText={(record) => record.id}
@@ -26,12 +32,36 @@ export const PostList = () => {
           tertiaryText={(record) => record.body}
         />
       ) : (
-        <Datagrid rowClick="edit">
+        
+        <Datagrid>
+            <ReferenceField source="userId" reference="users" />
             <TextField source="id" />
             <TextField source="title" />
             <TextField source="body" />
+            < EditButton />
         </Datagrid>
       )}
     </List>
   );
 };
+
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="id"  disabled/>
+            <TextInput source="title" />
+            <TextInput source="body"  multiline rows={5}/>
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = () => (
+    <Create>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="title" />
+            <TextInput source="body"  multiline rows={5}/>
+        </SimpleForm>
+    </Create>
+);
